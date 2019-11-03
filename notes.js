61ebc6d2dc296d5878f8a34fd07818778f32942b
Ryland Wang <rylandwang@gmail.com>
2019-11-03 08:32:12 -0500

    download function with set filename

--- notes.js
+++ notes.js
@@ -130,7 +130,44 @@ function finishRecording() {
   // console.log(notesPlayed)
   var outputString = ""
 
-  uriContent = "data:application/octet-stream;filename=masterpiece.txt," +
-    encodeURIComponent(notesPlayed);
-  newWindow = window.open(uriContent, 'masterpiece.txt');
+  // uriContent = "data:application/octet-stream;filename=masterpiece.txt," +
+  //   encodeURIComponent(notesPlayed);
+  // newWindow = window.open(uriContent, 'masterpiece.txt');
+
+  download(notesPlayed, "masterpiece.txt", "txt")
 }
+
+function download(strData, strFileName, strMimeType) {
+  var D = document,
+      a = D.createElement("a");
+      strMimeType= strMimeType || "application/octet-stream";
+
+
+  if (navigator.msSaveBlob) { // IE10
+      return navigator.msSaveBlob(new Blob([strData], {type: strMimeType}), strFileName);
+  } /* end if(navigator.msSaveBlob) */
+
+
+  if ('download' in a) { //html5 A[download]
+      a.href = "data:" + strMimeType + "," + encodeURIComponent(strData);
+      a.setAttribute("download", strFileName);
+      a.innerHTML = "downloading...";
+      D.body.appendChild(a);
+      setTimeout(function() {
+          a.click();
+          D.body.removeChild(a);
+      }, 66);
+      return true;
+  } /* end if('download' in a) */
+
+
+  //do iframe dataURL download (old ch+FF):
+  var f = D.createElement("iframe");
+  D.body.appendChild(f);
+  f.src = "data:" +  strMimeType   + "," + encodeURIComponent(strData);
+
+  setTimeout(function() {
+      D.body.removeChild(f);
+  }, 333);
+  return true;
+} /* end download() */
