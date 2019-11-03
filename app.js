b37d21711d8190e0616241f42abd9c409d84a0ad
Ryland Wang <rylandwang@gmail.com>
2019-11-03 01:32:57 -0400

    frequency class not needed

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 
-const Application = function() {
+const Application = function () {
   this.tuner = new Tuner()
   this.notes = new Notes('.notes', this.tuner)
   this.meter = new Meter('.meter')
   this.update({ name: 'A', frequency: 440, octave: 4, value: 69, cents: 0 })
 }
 
-Application.prototype.start = function() {
+Application.prototype.start = function () {
   const self = this
 
-  this.tuner.onNoteDetected = function(note) {
+  this.tuner.onNoteDetected = function (note) {
     if (self.notes.isAutoMode) {
       if (self.lastNote === note.name) {
         self.update(note)
@@ -18,31 +18,29 @@ Application.prototype.start = function() {
       }
     }
   }
-
-  swal('Welcome online tuner!').then(function() {
-    self.tuner.init()
-    self.frequencyData = new Uint8Array(self.tuner.analyser.frequencyBinCount)
-  })
+  
+  self.tuner.init()
+  self.frequencyData = new Uint8Array(self.tuner.analyser.frequencyBinCount)
 
   if (!/Android/i.test(navigator.userAgent)) {
     this.updateFrequencyBars()
   }
 }
 
-Application.prototype.updateFrequencyBars = function() {
+Application.prototype.updateFrequencyBars = function () {
   if (this.tuner.analyser) {
     this.tuner.analyser.getByteFrequencyData(this.frequencyData)
   }
   requestAnimationFrame(this.updateFrequencyBars.bind(this))
 }
 
-Application.prototype.update = function(note) {
+Application.prototype.update = function (note) {
   this.notes.update(note)
   this.meter.update((note.cents / 50) * 45)
 }
 
 // noinspection JSUnusedGlobalSymbols
-Application.prototype.toggleAutoMode = function() {
+Application.prototype.toggleAutoMode = function () {
   this.notes.toggleAutoMode()
 }
 
