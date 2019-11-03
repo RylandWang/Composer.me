47087ec33761ba661706ea43b4d5686f6b5d482c
Ryland Wang <rylandwang@gmail.com>
2019-11-03 01:31:25 -0500

    reduce note map index by 1

--- notes.js
+++ notes.js
@@ -76,18 +76,18 @@ Notes.prototype.update = function(note) {
     
     //account for half notes
     const wholeNoteMapping = {
-      1:1,
-      2:1,
-      3:2,
-      4:2,
-      5:3,
-      6:4,
-      7:4,
-      8:5,
-      9:5,
-      10:6,
-      11:6,
-      12:7
+      0:1,
+      1:1, //c#
+      2:2,
+      3:2, //d#
+      4:3,
+      5:4,
+      6:4, //f#
+      7:5,
+      8:5, //g#
+      9:6,
+      10:6, //a#
+      11:7
     }
     //update dot position in real time
     var updateYPosition = (14 - wholeNoteMapping[note.value%12]).toString() + "%";
